454074ac7cb9b6c5116ac0eacefe01e4b695ee67
1OWO-OMO1 <3022450775@qq.com>
2023-06-17 16:44:00 +0800

    新增类的打印

--- src/test/java/cn/lzy/ChapterTestApplication.java
+++ src/test/java/cn/lzy/ChapterTestApplication.java
@@ -1,6 +1,10 @@
 package cn.lzy;
 
+import cn.lzy.configbean.Person;
+import cn.lzy.configbean.Student;
+import cn.lzy.configbean.User;
 import cn.lzy.controller.ChapterController;
+import cn.lzy.customconfig.CustomProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +17,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ChapterTestApplication {
-    private String TAG = "ChapterTestApplication";
+    @Autowired
+    ChapterController chapterController;
 
     @Autowired
-    ChapterController ChapterController;
+    Student student;
+
+    @Autowired
+    Person person;
+
+    @Autowired
+    User user;
+
+    @Autowired
+    CustomProperties customProperties;
 
     @Test
-    public void getHello() {
-        String result = ChapterController.sayHello();
-        System.out.println(TAG + "----单元测试返回的数据11111111---------" + result);
+    public void contextLoads(){
+        System.out.println("单元测试返回="+chapterController.sayHello());
+        System.out.println("单元测试返回的student数据="+student);
+        System.out.println("单元测试返回的user数据=" + user);
+        System.out.println("单元测试返回的person数据=" + person);
+        System.out.println("单元测试返回的customProperties数据=" + customProperties);
     }
-
 }
