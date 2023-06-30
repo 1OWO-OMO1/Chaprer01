e04d25666fd8720dc9ba7af126054bac5f3aede5
1OWO-OMO1 <3022450775@qq.com>
2023-06-30 21:44:29 +0800

    第三章数据库开始

---
+++ src/main/java/cn/lzy/mybatiscatalog/CommentMapper.java
@@ -0,0 +1,10 @@
+package cn.lzy.mybatiscatalog;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface CommentMapper {
+    @Select("SELECT * FROM t_comment WHERE id =#{id}")
+    public Comment findById(Integer id);
+}
--- src/test/java/cn/lzy/ChapterTestApplication.java
+++ src/test/java/cn/lzy/ChapterTestApplication.java
@@ -7,6 +7,8 @@ import cn.lzy.controller.ChapterController;
 import cn.lzy.customconfig.CustomProperties;
 import cn.lzy.customconfig.DBConnector;
 import cn.lzy.customconfig.MyProperties;
+import cn.lzy.mybatiscatalog.Comment;
+import cn.lzy.mybatiscatalog.CommentMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,21 +43,26 @@ public class ChapterTestApplication {
     @Autowired
     DBConnector dbConnector;
 
+    @Autowired
+    private CommentMapper commentMapper;
+
 
 
     @Test
     public void contextLoads(){
+        Comment comment = commentMapper.findById(1);
+        System.out.println(""+comment);
         //System.out.println("单元测试返回="+chapterController.sayHello());
         //System.out.println("单元测试返回的student数据="+student);
         //System.out.println("单元测试返回的user数据=" + user);
         //System.out.println("单元测试返回的person数据=" + person);
         //System.out.println("单元测试返回的customProperties数据=" + customProperties);
-        MyProperties myProperties = (MyProperties) applicationContext.getBean("myProperties");
-        if (myProperties==null){
-            System.out.println("myProperties类注入不成功");
-        }else {
-            myProperties.getResult();
-        }
-        System.out.println(dbConnector.config());
+        //MyProperties myProperties = (MyProperties) applicationContext.getBean("myProperties");
+        //if (myProperties==null){
+        //    System.out.println("myProperties类注入不成功");
+        //}else {
+          //  myProperties.getResult();
+        //}
+        //System.out.println(dbConnector.config());
     }
 }
