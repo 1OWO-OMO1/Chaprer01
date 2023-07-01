2d1e2df70009a1dc3441d9da71d53a5b6567c3f3
1OWO-OMO1 <3022450775@qq.com>
2023-07-01 17:22:15 +0800

    第三章数据库第一次作业完成

--- src/main/java/cn/lzy/mybatiscatalog/CommentMapper.java
+++ src/main/java/cn/lzy/mybatiscatalog/CommentMapper.java
@@ -1,10 +1,27 @@
 package cn.lzy.mybatiscatalog;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface CommentMapper {
     @Select("SELECT * FROM t_comment WHERE id =#{id}")
     public Comment findById(Integer id);
+
+/*    @Insert("INSERT INTO t_comment(content,author,a_id) " +
+            "values (#{content},#{author},#{aId})")
+    public int insertComment(Comment comment);//插入*/
+
+    @Update("UPDATE t_comment SET content=#{content} WHERE id=#{id}")
+    public int updateComment(Comment comment);//更新
+
+    @Delete("DELETE FROM t_comment WHERE id=#{id}")
+    public int deleteComment(Integer id);//删除
+
+    @Select("SELECT * FROM t_comment WHERE author =#{id}")
+    public Comment czauther(String author);
+
+    @Update("UPDATE t_comment SET content=#{content} WHERE author=#{author}")
+    public int gxauthor(Comment comment);//更新
 }
+
+
--- src/test/java/cn/lzy/ChapterTestApplication.java
+++ src/test/java/cn/lzy/ChapterTestApplication.java
@@ -47,11 +47,44 @@ public class ChapterTestApplication {
     private CommentMapper commentMapper;
 
 
-
     @Test
     public void contextLoads(){
-        Comment comment = commentMapper.findById(1);
-        System.out.println(""+comment);
+ /*       Comment comment = commentMapper.findById(1);
+        System.out.println(""+comment);*/
+
+        Comment comment4 = commentMapper.czauther("张三");
+        System.out.println(""+comment4);
+
+        Comment comment2=new Comment();
+        comment2.setAuthor("卡片");
+        comment2.setContent("小航");
+        commentMapper.gxauthor(comment2);
+        System.out.println("更新成功--"+comment2);//更新
+
+     /*   Comment comment5 = new Comment();
+        comment5.setAuthor("卡片");
+        comment5.setContent("小明");
+        commentMapper.gxauthor(comment5);
+        System.out.println("通过author 字段去更新成功--"+comment5);*/
+
+        //Comment comment1=new Comment();
+        //comment1.setContent("小明");
+        //comment1.setAuthor("卡片");
+        //comment1.setA_id(1);
+        //commentMapper.insertComment(comment1);
+        //System.out.println("插入成功--"+comment1);//插入
+
+   /*     Comment comment2=new Comment();
+        comment2.setId(7);
+        comment2.setContent("小刚");
+        commentMapper.updateComment(comment2);
+        System.out.println("更新成功--"+comment2);//更新
+
+        Comment comment3=new Comment();
+        commentMapper.deleteComment(8);
+        System.out.println("删除成功");*/
+
+
         //System.out.println("单元测试返回="+chapterController.sayHello());
         //System.out.println("单元测试返回的student数据="+student);
         //System.out.println("单元测试返回的user数据=" + user);
